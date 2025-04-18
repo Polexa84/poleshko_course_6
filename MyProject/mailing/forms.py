ed54f56ce02485aa9f42e671ddc25e5efc959d1f
polexa84 <polexa84@gmail.com>
2025-04-19 01:34:19 +0300

    ПОправил форму для заполнения к красивому визуалу

--- MyProject/mailing/forms.py
+++ MyProject/mailing/forms.py
@@ -5,3 +5,13 @@ class RecipientForm(forms.ModelForm):
     class Meta:
         model = Recipient
         fields = ['email', 'full_name', 'comment']
+        help_texts = {
+            'email': 'Введите корректный email адрес.',
+            'full_name': 'Введите ваше полное имя.',
+            'comment': 'Здесь можно оставить любой комментарий.',
+        }
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'full_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'comment': forms.Textarea(attrs={'class': 'form-control'}),
+        }
--- MyProject/mailing/models.py
+++ MyProject/mailing/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
 class Recipient(models.Model):
-    email = models.EmailField(unique=True)
-    full_name = models.CharField(max_length=255)
-    comment = models.TextField(blank=True)
+    email = models.EmailField(unique=True, verbose_name="Email")
+    full_name = models.CharField(max_length=255, verbose_name="Ф.И.О.")
+    comment = models.TextField(blank=True, null=True, verbose_name="Комментарий")
 
     def __str__(self):
-        return self.full_name
+        return self.email
+
+    class Meta:
+        verbose_name = "Получатель"
+        verbose_name_plural = "Получатели"
